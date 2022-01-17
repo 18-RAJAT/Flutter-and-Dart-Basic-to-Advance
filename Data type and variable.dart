07eb9babbca9170c29ecb3734c30c8fbb72c5cc5
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-01-18 03:18:11 +0530

    Add files via upload

---
+++ Data type and variable.dart
@@ -0,0 +1,47 @@
+//dart is supported 2 data type -> int,double (all data types in dart are objects and therefore,values are by default NULL)
+//>string and booleans
+
+
+void main()
+{
+
+                       int hexValue = 0xEADEBAEE; //random value
+  //Numbers : int
+  int score = 23;
+  var scoree = 32;
+  //Numbers : double
+  double percentage = 93.1;
+  var percentagee = 82.2;
+
+                         double  exponential = 1.42e5; //random value
+  //string : 
+  String name = "harry";
+  var namee = "RAJ";
+  //boolean
+  bool isvalid = true;
+  var isinvalid = false;
+
+  
+  print(score);
+  print(percentage);
+  print(name);
+  print(isvalid) ;
+
+  print("\n");
+  
+  
+  print(scoree);
+  print(percentagee);
+  print(namee);
+  print(isinvalid);
+
+  print("\n");
+
+  print(hexValue);
+  print(exponential);
+
+
+   //this ection will be written as null...!!
+  var user_name;
+  print(user_name);  
+}
