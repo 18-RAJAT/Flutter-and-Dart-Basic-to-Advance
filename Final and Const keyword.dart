eecd2525cbedfe3b1d7428623089f99174c18945
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-01-18 11:07:13 +0530

    Add files via upload

---
+++ Final and Const keyword.dart
@@ -0,0 +1,18 @@
+/* 
+    FINAL... this variable can only be set once and it is initialized when accessed
+    CONSt... this variable is implicitily final but it is a compile time constant
+*/
+
+void main()
+{
+  //Final keyword 
+  final cityName = 'delhi';
+
+  final String countryName = 'india';
+
+  //Const keyword
+  const PI = 3.14;
+  const gravitiy = 9.8;
+
+  print(PI);
+}
