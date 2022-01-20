3085f8a9e8aa6919ba95352a842404a8c953eccc
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-01-20 12:37:56 +0530

    Add files via upload

---
+++ Continue statement.dart
@@ -0,0 +1,36 @@
+// void main()
+// {
+//   for (int i=1;i<=10;++i)
+//   {
+//     //print odd numbers
+//     if(i % 2 == 0)
+//     {
+//       continue;
+//     }
+
+//     // if(i == 4)
+//     // {
+//     //   //4 is skipped...!!
+//     //   continue;
+//     // }
+//     // print(i);
+//   }
+// }
+
+
+
+//Nesting
+void main ()
+{
+  for(int i=0;i<=3;++i)
+  {
+    for(int j=0;j<=2;++j)
+    {
+      if( i == 2 && i == 2)
+      {
+        continue;
+      }
+       print("$i $j");
+    }
+  }
+}
