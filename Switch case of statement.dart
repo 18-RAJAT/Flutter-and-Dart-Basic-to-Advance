8a56b14309fe42e4c9bf4985c9b7b82f1c036671
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-01-20 11:22:13 +0530

    Add files via upload

---
+++ Switch case of statement.dart
@@ -0,0 +1,25 @@
+void main()
+{
+  String grade = 'B';
+  switch(grade)
+  {
+    case 'A':
+    print("Awesome");
+    break;
+
+    case 'B':
+    print("Nice");
+    break;
+
+    case 'C':
+    print("Work hard!");
+    break;
+    
+    case 'D':
+    print("Failed");
+    break;
+
+    default:
+    print("Invalid Grade");
+  }
+}
