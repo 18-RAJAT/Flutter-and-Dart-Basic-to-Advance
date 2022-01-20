e3d18dac637f200db90d0f673339fa3d92171f43
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-01-20 12:26:37 +0530

    Add files via upload

---
+++ Break statement.dart
@@ -0,0 +1,29 @@
+void main()
+{
+  for(int i=0;i<=10;++i)
+  {
+    print(i);
+
+    if(i == 5)
+    {
+      break;
+    }
+  }
+
+}
+
+void main()
+{
+  for(int i=0;i<=4;++i)
+  {
+    for(int j=0;j<=4;++j)
+   {
+    print("$i $j");
+
+    if(i == 2 && j == 2)
+    {
+      break;
+    }
+  }
+  }
+}
