77f35eef9083c7d415141e5a73d3f82d3ad7272b
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-01-20 11:51:14 +0530

    Add files via upload

---
+++ Loops.dart
@@ -0,0 +1,48 @@
+
+/*
+   Loops 1.Definite ->>For loop
+         2.Indefinite ->> while loop, Do - While loop
+*/
+
+//For loop
+void main()
+{
+  int i=0;
+  for(int i=0;i<4;++i)
+  {
+    print("Hello");
+  }
+  print("\n");
+  {
+    print("exit");
+  }
+}
+
+
+//While loop
+
+void main()
+{
+  int i=0;
+  while(i<5)
+  {
+    print("Hello");
+    i++;
+  }
+}
+
+
+
+//Do While
+
+void main()
+{
+  int i=0;
+  do{
+    print("Hello");
+    i++;
+  }
+
+  while(i<5);
+}
+
