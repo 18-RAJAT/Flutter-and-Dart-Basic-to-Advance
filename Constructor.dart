12a3d4c74c37b00a02505636c7f0fd53b3080cb8
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-01-23 15:33:04 +0530

    Add files via upload

---
+++ Constructor.dart
@@ -0,0 +1,18 @@
+void main()
+{
+  Dart().dartFunction();
+}
+
+class Dart
+{
+ 
+  Dart()
+  {
+    print("This is Constructor");
+  }
+
+  void dartFunction()
+  {
+    print("Function call successfully");
+  }
+}
